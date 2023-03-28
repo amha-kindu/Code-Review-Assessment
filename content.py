04c2e2fcf4886f48c0d983be5f4d86eb33818faf
Zelalem Yetsedaw <zy94988@gmail.com>
2023-03-28 11:21:27 +0300

    add comment

--- content.py
+++ content.py
@@ -1,6 +1,8 @@
+# function to calculate area of circle
 def calculateArea(radius):
     PI = 3.14
     return PI * radius ** 2
 
+# function to divide two numbers
 def divide_numbers(a, b):
     return a / b
